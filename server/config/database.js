18b45a30ea141fef505029d08edc212fa6c0cf6f
Bhushan Bankar <bankarbhushan18@gmail.com>
2025-05-16 10:58:04 +0530

    last

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.connect("mongodb://localhost:27017/Billsystem");
+  // mongoose.connect("mongodb://localhost:27017/Billsystem");
+  mongoose.connect(
+    "mongodb+srv://Bhushan:bhushan@cluster0.fxsvhhy.mongodb.net/Billsystem"
+  );
 };
 
 module.exports = connectDB;
